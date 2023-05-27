ebe440cb1b86b7bf2e3b8dbd8c122626f5bc07e1
BrunoBosshard <bruno.bosshard@pepgo.com>
2023-05-27 12:36:18 +1000

    Updated cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,6 @@ const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
   e2e: {
-    // baseUrl, etc
-    // supportFile: 'cypress/support/e2e.js',
-    fixturesFolder: false,
     setupNodeEvents(on, config) {
       cypressSplit(on, config)
       // IMPORTANT: return the config object
